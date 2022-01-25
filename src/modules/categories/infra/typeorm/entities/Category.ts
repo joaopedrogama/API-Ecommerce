e1f94549298f19234544142d987a236909613302
João Pedro Gama <joaocletto@gmail.com>
2022-01-25 18:07:14 -0300

    product

--- src/modules/categories/infra/typeorm/entities/Category.ts
+++ src/modules/categories/infra/typeorm/entities/Category.ts
@@ -1,22 +1,27 @@
+import Product from '../../../../products/infra/typeorm/entities/Product';
 import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('categorias')
 export default class Category {
-    @PrimaryGeneratedColumn('increment')
-    id: number;
+  @PrimaryGeneratedColumn('increment')
+  id: number;
 
-    @Column()
-    descricao: string;
+  @Column()
+  descricao: string;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    @UpdateDateColumn()
-    updated_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @OneToMany(() => Product, (product) => product.categoria)
+  produtos: Product[];
 }
