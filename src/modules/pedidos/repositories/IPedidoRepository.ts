36468310d8965c5f747ba74598e7bc93e9eb4a72
João Pedro Gama <joaocletto@gmail.com>
2022-01-27 21:05:56 -0300

    começando repositorio

---
+++ src/modules/pedidos/repositories/IPedidoRepository.ts
@@ -0,0 +1,8 @@
+import IPedidoDTO from '../dtos/IPedidoDTO';
+import Pedido from '../infra/typeorm/entities/Pedido';
+
+export default interface IPedidoRepository {
+  doOrder(data: IPedidoDTO): Promise<Pedido>;
+
+  ClientOrders(clientId: number): Promise<Pedido>;
+}
